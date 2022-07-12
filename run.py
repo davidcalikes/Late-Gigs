9077855f528b16428e7004bb9b48dfcab9417a56
davidcalikes <davidcalikes@gmail.com>
2022-07-12 10:58:28 +0000

    feat: Add menu function options via while loop

--- run.py
+++ run.py
@@ -17,13 +17,30 @@ def main():
     """
     Display Welcome message and get user type via menu options
     """
-    print("\nWelcome to Late Gigs!")
-    print('The Last-Minute Booking Service for Live Music!\n')
-    print('1. Find an Artist')
-    print('2. Find A Venue')
-    print('3. Exit\n')
-    print('Choose the number from the options above and press enter')
-    exit()
+    while True:
+        print("\nWelcome to Late Gigs!")
+        print('The Last-Minute Booking Service for Live Music!\n')
+        print('1. Find an Artist')
+        print('2. Find A Venue')
+        print('3. Exit\n')
+        print('Choose the number from the options above and press enter')
+
+        user_option = input('Enter your choice here: ')
+
+        if user_option == '1':
+            print('\nFind an Artist... Ok Great! Lets Get started!\n')
+            user_is = "venue"
+            area_check(user_is)
+        elif user_option == '2':
+            print('\nFind a Venue... Ok Great! Lets Get started!\n')
+            user_is = "artist"
+            area_check(user_is)
+        elif user_option == '3':
+            print('\nBetter luck next time... Be sure to check back soon!')
+            exit()
+        else:
+            print('\nInvalid option! Please type either 1, 2, or 3\n')
+            input('Press Enter to try again...\n')
 
 
 main()
