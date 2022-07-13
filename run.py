570271927862dc7efe1847477ef8f0aee50e9738
davidcalikes <davidcalikes@gmail.com>
2022-07-13 15:21:55 +0000

    feat: Copy get_venue_data for get_act_data framework

--- run.py
+++ run.py
@@ -200,6 +200,151 @@ def get_venue_data():
     exit()
 
 
+    def get_act_data():
+    """
+    Get data from the user incrementally
+    that lists the properties of the act object.
+    Validates data at every input to improve UX and
+    reduce risk of TypeErrors while working with
+    a list containing different data types.
+    """
+    act_data = []
+
+    while True:
+        name = input("Enter the name of your act here: \n")
+        if len(name) >= 2:
+            act_data.append(name.lower())
+            break
+        else:
+            clear_page()
+            print("\n")
+            print(f'{name} is not a valid name')
+            print("\nAct names must contain more than")
+            print("two characters!")
+            print("\nPlease Try Again!\n")
+            continue
+
+    print("\n")
+    print(f"Cool! What genre of music does {name} mostly play?")
+    print("\n")
+    print("Type one of the following options:")
+    print("Rock, Blues, Pop, Jazz, Metal, R&b, Indie, Country, Irish trad \n")
+    genre_list = ["Rock", "rock", "ROCK", "Blues", "blues", "BLUES",
+                  "Pop", "pop", "POP", "Jazz", "jazz", "JAZZ", "Metal",
+                  "metal", "METAL", "R&b", "r&b", "R&B", "Indie", "indie",
+                  "Country", "country", "COUNTRY",
+                  "Irish Trad", "irish trad", "IRISH TRAD"]
+    while True:
+        genre = input("Enter genre here: \n")
+        if genre in genre_list:
+            venue_data.append(genre.lower())
+            break
+        else:
+            clear_page()
+            print("\n")
+            print(f"{genre} is not a valid genre")
+            print("\nType one of the following options:")
+            print("\nRock, Blues, Pop, Jazz, Metal, R&b,")
+            print("Indie, Country or Irish trad \n")
+            continue
+
+    clear_page()
+    print("\nNice! Hopefully your patrons will be enjoying some")
+    print(f"live {genre} at {name} soon!")
+    print("\n")
+
+    print("Let's keep going!\n")
+    print("What day this weekend do you need the act?")
+    print("Type: Friday, Saturday or Sunday \n")
+    day_list = ["Friday", "friday", "FRIDAY",
+                "Saturday", "saturday", "SATURDAY",
+                "Sunday", "sunday", "SUNDAY"]
+    while True:
+        day = input("Enter required day: \n")
+        if day in day_list:
+            venue_data.append(day.lower())
+            break
+        else:
+            clear_page()
+            print("\n")
+            print(f"{day} is not a valid gig day... Try again!")
+            print("\nType one of the following options:\n")
+            print("Friday, Saturday or Sunday")
+            continue
+    clear_page()
+    print("\nExcellent! So far so good?")
+    print("Here's what we have so far...\n")
+    print(f"Your venue: '{name}' is looking for a {genre}")
+    print(f"act for this coming {day}?")
+    print("\n")
+
+    while True:
+        data_correct = input("Are you happy to continue?:(y/n)\n")
+        if data_correct == "y":
+            clear_page()
+            print("\nOK cool! Now let's keep going!\n")
+            break
+        else:
+            clear_page()
+            print("That's ok, let's try again\n")
+            get_venue_data()
+            main()
+
+    print("Tell us the maximum fee you are willing to pay your act.\n")
+    print("Hint: Act fees vary depending on many factors, such as")
+    print("length of set, number of band members ect.\n")
+    print("Aim to set your maximum fee between €200 and €600 to increase")
+    print("your chances of finding a suitable act\n")
+    while True:
+        try:
+            fee = int(input("Enter max fee in € here: \n"))
+        except ValueError:
+            clear_page()
+            print("Nope! We need a number here... try again!")
+            continue
+        else:
+            break
+
+    venue_data.append(fee)
+    clear_page()
+    print("\nOutstanding! We're almost there!\n")
+
+    print("What is the maximum number of performers")
+    print(f"the stage at {name} can hold?")
+    print("\n")
+    while True:
+        try:
+            members = int(input(
+                "Type a number between 1 and the maximum required: \n"))
+        except ValueError:
+            clear_page()
+            print("Sorry, only a number will do in this case. Try again!")
+            continue
+        else:
+            break
+
+    venue_data.append(members)
+    clear_page()
+
+    print("\nExcellent! Ok, just one last thing!\n")
+    print("How long should the act play for?")
+    print("\nExample: A two and a half hour set would be: 2.5 \n")
+    while True:
+        try:
+            set_length = float(input("Enter length of set required: \n"))
+        except ValueError:
+            clear_page()
+            print("Whoopsie! This value needs to be a number. Try again!")
+            continue
+        else:
+            break
+    venue_data.append(set_length)
+    clear_page()
+    print(venue_data)
+
+    exit()
+
+
 def main():
     """
     Display Welcome message and get user type via menu options
